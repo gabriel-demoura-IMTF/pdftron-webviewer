68984fc0475807f09e7a6a44e138b3d46502b723
gabriel-demoura-IMTF <gabriel.demoura@imtf.com>
2026-01-09 16:36:51 +0000

    Finding out how to disable form inputs in view mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,90 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import WebViewer from "@pdftron/webviewer";
 import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
   const instanceRef = useRef(null);
+
+  const getCurrentToolbarGroup = useCallback(
+    () =>
+      document
+        .getElementsByTagName("apryse-webviewer")[0]
+        ?.shadowRoot?.querySelector(".RibbonGroup .active")
+        ?.attributes?.getNamedItem("data-element")?.value,
+    []
+  );
+
+  const handleToolbarGroupChange = useCallback(
+    (e?: { detail: string }) => {
+      if (instanceRef.current) {
+        const { annotationManager } = instanceRef.current.Core;
+        const { ToolbarGroup } = instanceRef.current.UI;
+        const fieldManager = annotationManager.getFieldManager();
+        const fields = fieldManager.getFields();
+        const annotationsList = annotationManager.getAnnotationsList();
+        const currentToolbarGroup = e?.detail || getCurrentToolbarGroup();
+
+        const isViewModeEnabled = currentToolbarGroup === ToolbarGroup.VIEW;
+
+        // Disable edition for form files when using the view mode toolbar group
+        fields.forEach((field) => {
+          field.widgets?.forEach((widgetParam) => {
+            const widget = widgetParam;
+            if (widget.element) {
+              if (isViewModeEnabled) {
+                (widget.element as HTMLElement).style.setProperty(
+                  "pointer-events",
+                  "none"
+                );
+              } else {
+                (widget.element as HTMLElement).style.removeProperty(
+                  "pointer-events"
+                );
+              }
+            } else {
+              if (isViewModeEnabled) {
+                widget.ReadOnly = true;
+              } else {
+                widget.ReadOnly = false;
+              }
+            }
+            // Disables tabs
+            if (widget.innerElement) {
+              (widget.innerElement as HTMLElement).tabIndex = isViewModeEnabled
+                ? -1
+                : 0;
+            }
+          });
+        });
+
+        // Disable edition for annotations when using the view mode toolbar group
+        annotationsList.forEach((annotParam) => {
+          const annot = annotParam;
+          annot.ReadOnly = isViewModeEnabled;
+
+          if (
+            instanceRef.current &&
+            annot instanceof
+              instanceRef.current.Core.Annotations.WidgetAnnotation &&
+            annot.element
+          ) {
+            if (isViewModeEnabled) {
+              (annot.element as HTMLElement).style.setProperty(
+                "pointer-events",
+                "none"
+              );
+            } else {
+              (annot.element as HTMLElement).style.removeProperty(
+                "pointer-events"
+              );
+            }
+          }
+        });
+      }
+    },
+    [getCurrentToolbarGroup]
+  );
 
   useEffect(() => {
     WebViewer.WebComponent(
@@ -19,49 +99,38 @@ const App = () => {
 
       const { documentViewer, annotationManager } = instance.Core;
 
-      documentViewer.addEventListener("annotationsLoaded", () => {
-        const annotationsList = annotationManager.getAnnotationsList();
+      const currentToolbarGroup = getCurrentToolbarGroup();
 
-        annotationsList.forEach((annotParam) => {
-          const annot = annotParam;
-          annot.ReadOnly = false;
-          // It the same when using "true"
-          // annot.ReadOnly = true;
-        });
+      const { ToolbarGroup } = instanceRef.current.UI;
+      const isViewModeEnabled = currentToolbarGroup === ToolbarGroup.VIEW;
+
+      // Even read only mode enabled doesn't work
+      // if (isViewModeEnabled) {
+      //   annotationManager.enableReadOnlyMode();
+      // } else {
+      //   annotationManager.disableReadOnlyMode();
+      // }
+
+      documentViewer.addEventListener("pageComplete", () => {
+        handleToolbarGroupChange();
       });
+
+      // We need to wait for annotations to be able to know if we should display the collect signature custom button
+      documentViewer.addEventListener("annotationsLoaded", () => {
+        handleToolbarGroupChange();
+      });
+
+      instance.UI.addEventListener(
+        "toolbarGroupChanged",
+        handleToolbarGroupChange
+      );
     });
   }, []);
-
-  const handleSave = () => {
-    const instance = instanceRef.current;
-    const { documentViewer, annotationManager } = instance.Core;
-
-    return Promise.all([
-      annotationManager.exportAnnotations(),
-      documentViewer.getDocument(),
-    ])
-      .then(([xfdfString, document]) =>
-        document.getFileData({
-          xfdfString,
-        })
-      )
-      .then((buffer) => {
-        const arr = new Uint8Array(buffer);
-        const data = new Blob([arr], {
-          type: "application/pdf",
-        });
-        const file = new File([data], "checkbox.pdf");
-
-        instance.UI.loadDocument(file);
-      });
-  };
 
   return (
     <>
       <div className="App">
-        <div className="header">
-          React sample <button onClick={handleSave}>Save</button>
-        </div>
+        <div className="header">React sample</div>
         <div className="webviewer" ref={viewer}></div>
       </div>
     </>
